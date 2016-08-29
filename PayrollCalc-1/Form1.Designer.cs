2ac5fce375eec955f5d9afd940372cb31c9e2e71
Zachary Betters <zachbet97@outlook.com>
2016-08-29 09:22:58 -0400

    Add project files.

---
+++ PayrollCalc-1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PayrollCalc_1
+{
+    partial class Payroll_Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Payroll_Calc));
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.tbHoursWorked = new System.Windows.Forms.TextBox();
+            this.tbHourlyRate = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.AutoSize = true;
+            this.lblHoursWorked.Location = new System.Drawing.Point(24, 51);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(79, 13);
+            this.lblHoursWorked.TabIndex = 0;
+            this.lblHoursWorked.Text = "Hours Worked:";
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.AutoSize = true;
+            this.lblHourlyRate.Location = new System.Drawing.Point(25, 75);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(87, 13);
+            this.lblHourlyRate.TabIndex = 1;
+            this.lblHourlyRate.Text = "Hourly Pay Rate:";
+            // 
+            // tbHoursWorked
+            // 
+            this.tbHoursWorked.Location = new System.Drawing.Point(140, 48);
+            this.tbHoursWorked.Name = "tbHoursWorked";
+            this.tbHoursWorked.Size = new System.Drawing.Size(125, 20);
+            this.tbHoursWorked.TabIndex = 2;
+            // 
+            // tbHourlyRate
+            // 
+            this.tbHourlyRate.Location = new System.Drawing.Point(140, 72);
+            this.tbHourlyRate.Name = "tbHourlyRate";
+            this.tbHourlyRate.Size = new System.Drawing.Size(125, 20);
+            this.tbHourlyRate.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(28, 109);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 36);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(109, 109);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(190, 109);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 36);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            // 
+            // Payroll_Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(295, 207);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbHourlyRate);
+            this.Controls.Add(this.tbHoursWorked);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Name = "Payroll_Calc";
+            this.Text = "Payroll Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHoursWorked;
+        private System.Windows.Forms.Label lblHourlyRate;
+        private System.Windows.Forms.TextBox tbHoursWorked;
+        private System.Windows.Forms.TextBox tbHourlyRate;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ PayrollCalc-1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+/* Author: Zachar Betters
+ * Date: 24 August, 2016
+ * Class: CIS 209
+ * Teacher: Eric Mailman
+ * Title: Payroll Calculator 1
+ * Description: This program will calculate the gross pay of the given data. 
+ */
+
+
+namespace PayrollCalc_1 {
+    public partial class Payroll_Calc : Form {
+        public Payroll_Calc() { InitializeComponent(); }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+     
+        private void btnExit_Click_1(object sender, EventArgs e) { Close(); }
+
+        private void btnCalc_Click_1(object sender, EventArgs e) {
+            decimal hourlyRate, grossPay, hoursWorked;  //declares these variables to be decimals
+
+            if (decimal.TryParse(tbHoursWorked.Text, out hoursWorked) &&   //if the entered data are decimals 
+                    decimal.TryParse(tbHourlyRate.Text, out hourlyRate))
+            {
+
+                grossPay = hourlyRate * hoursWorked; //calculattes answer and outputs it in a message box
+                MessageBox.Show("The gross pay is: $" + grossPay, "Pay");
+
+            }
+            else //if entered data is blank or something other than a number 
+                MessageBox.Show("You must enter data in order for me to calculate it!", "Error");
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e) {
+            tbHourlyRate.Text = String.Empty;
+            tbHoursWorked.Text = String.Empty;
+        }
+    }
+}
